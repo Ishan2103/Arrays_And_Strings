d91bd33768a49d13c78c3f1052eacad06efbe0b1
Ishan2103 <ishanextra21@gmail.com>
2025-08-06 15:10:50 +0530

    Create string as input and display.cpp

---
+++ string as input and display.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char text[50];
+
+    cout << "Enter a string: ";
+    cin >> text;  
+    cout << "You entered: " << text << endl;
+
+    return 0;
+}
